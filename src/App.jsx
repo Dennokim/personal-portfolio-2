9941eadbbfb735bdc5d4a049943dd7142e2fb610
Denis <deniskimani2022@gmail.com>
2023-08-07 17:01:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+import Header from "./components/Header"
+import Aboutphoto from "./components/Aboutphoto"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Aboutphoto/>
+    </>
+  )
+}
